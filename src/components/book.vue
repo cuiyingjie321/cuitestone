ac9151e5dd80352a2c2fae11e5aa1b8f081bf053
毛小喵 <plecun@users.noreply.github.com>
2018-06-14 11:04:55 +0800

    6666

--- src/components/book.vue
+++ src/components/book.vue
@@ -183,7 +183,7 @@ export default {
 	},
 	getbookinfo: function () {
 		this.$http.get("/wap/book",{params:{book_id:this.$route.query.bookid}}).then(function(res){
-			this.dataInfo  	= res.data5555
+			this.dataInfo  	= res.data6666
 			this.dataDetail 	= this.dataInfo.data
 			this.chapterInfo 	= this.dataDetail.books.chapter_info			
 		},function(res){  
